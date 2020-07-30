0fe6e7419784a655dda1e5d63a10d7dc22dac04e
Carsten <secret>
2020-07-30 11:59:03 -0700

    re-added Secrets.h

---
+++ firmware/Prototype_v4/ESP32_Apollo_V4/Secrets.h
@@ -0,0 +1,10 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define WIFI_SSID "Your SSID"
+#define WIFI_PASS "Your PWD"
+
+// Minimum 8 characters !!
+#define WIFI_ACCESS_POINT_PASSWORD "Apollo42" 
+
+#endif // SECRETS_H
